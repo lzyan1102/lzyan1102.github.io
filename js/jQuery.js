6480fbbb59cf433b54cf091f2ffa74ac070c7023
lzyan1102 <lzyan1102@163.com>
2016-05-13 19:05:28 +0800

    Update jQuery.js

--- js/jQuery.js
+++ js/jQuery.js
@@ -1,20 +1,25 @@
-        function load()
+
+       function load()
         {
-        	//document.getElementById("img").src="pic/1.jpg";
-        	var num=Math.floor(Math.random()*4)
-        	document.getElementById("img").src="pic/"+num+".jpg";
-        	setTimeout("change(0)",3000);
+        	var num=Math.floor(Math.random()*4)+1;
+        	/*document.getElementById("img").src="pic/0.jpg";*/
+        	document.getElementById("A").setAttribute("background","pic/"+num+".jpg")
+        	document.getElementById("A").style.backgroundSize="contain";
+        	setTimeout("change()",3000);
         }
-        function change(n)
+        function change()
         {
         	if(n>4)
         	{
         		n=0;
         	}
-        		document.getElementById("img").setAttribute("src","pic/"+n+".jpg")
+        	/*document.getElementById("A").style.backgroundImage="url(pic/"+Math.floor(Math.random()*4+1+".jpg)";
+        	document.getElementById("A").style.backgroundSize="contain";*/
+        	var num=Math.floor(Math.random()*4)+1;
+        	document.getElementById("A").setAttribute("background","pic/"+num+".jpg")
+        	/*document.getElementById("img").setAttribute("src","pic/"+Math.floor(Math.random()*4)+1+".jpg");*/
         		n++;
-        		setTimeout("change("+n+")",3000);
-        	
+        		setTimeout("change()",3000);
         }
         function  JumpToweibo()
         {
