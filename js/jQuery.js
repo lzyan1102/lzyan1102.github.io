044a4076a1d40b8188522ff81bc4a8a1abf462c3
lzyan1102 <lzyan1102@163.com>
2016-05-13 19:21:39 +0800

    Update jQuery.js

--- js/jQuery.js
+++ js/jQuery.js
@@ -5,19 +5,15 @@
         	/*document.getElementById("img").src="pic/0.jpg";*/
         	document.getElementById("A").setAttribute("background","pic/"+num+".jpg")
         	document.getElementById("A").style.backgroundSize="contain";
-        	setTimeout("change(1)",3000);
+        	setTimeout("change()",3000);
         }
-        function change(n)
+        function change()
         {
-        	if(n>4)
-        	{
-        		n=1;
-        	}
+        
         	var num=Math.floor(Math.random()*4)+1;
         	document.getElementById("A").setAttribute("background","pic/"+num+".jpg")
         	/*document.getElementById("img").setAttribute("src","pic/"+Math.floor(Math.random()*4)+1+".jpg");*/
-        	n++
-        		setTimeout("change(n)",3000);
+        	setTimeout("change()",3000);
         }
         function  JumpToweibo()
         {
