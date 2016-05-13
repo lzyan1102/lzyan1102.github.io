0cb565cbfee60a9c66cfd99ad2509e26d81e3d1d
lzyan1102 <lzyan1102@163.com>
2016-05-13 09:58:55 +0800

    Update jQuery.js

--- js/jQuery.js
+++ js/jQuery.js
@@ -1,3 +1,21 @@
+        function load()
+        {
+        	document.getElementById("img").src="pic/1.jpg";
+        	//var num=Math.floor(Math.random()*4)
+        	//document.getElementById("img").src="pic/"+num+".jpg";
+        	setTimeout("change(0)",2000);
+        }
+        function change(n)
+        {
+        	if(n>4)
+        	{
+        		n=0;
+        	}
+        		document.getElementById("img").setAttribute("src","pic/"+n+".jpg")
+        		n++;
+        		setTimeout("change("+n+")",3000);
+        	
+        }
         function  JumpToweibo()
         {
         	var system ={};  
@@ -9,7 +27,77 @@
 			{//pc 
      		  window.location.href="http://www.weibo.com/u/3865190713/";  
    			 }else
-			{//ÊÖ»ú
+			{//moble
      		  window.location.href="http://m.weibo.cn/u/3865190713/";  
    			 }
        }
+        function videoone()
+        {
+        	document.getElementById("video").autoplay="autoplay"
+        	document.getElementById("video").src="http://gslb.miaopai.com/stream/VficKv5kY7oke~-Ix9kPnQ__.mp4?vend=miaopai&VficKv5kY7oke~-Ix9kPnQ__.mp4"
+        }
+        function videotow()
+        {
+        	document.getElementById("video").autoplay="autoplay"
+        	document.getElementById("video").src="http://gslb.miaopai.com/stream/vlxccaJRtZJfvWidASnzlg__.mp4?vend=miaopai&vlxccaJRtZJfvWidASnzlg__.mp4"
+        }
+         function videothree()
+        {
+        	document.getElementById("video").autoplay="autoplay"
+        	document.getElementById("video").src="http://gslb.miaopai.com/stream/4Fo~3JLM1uhyH7Yqz4huAQ__.mp4"
+        }
+         function videofour()
+        {
+        	document.getElementById("video").autoplay="autoplay"
+        	document.getElementById("video").src="http://gslb.miaopai.com/stream/NNOTi8M3YQn-K75aNF2WdQ__.mp4"
+        }
+         function videofive()
+        {
+        	document.getElementById("video").autoplay="autoplay"
+        	document.getElementById("video").src=""
+        }
+         function videosix()
+        {
+        	document.getElementById("video").autoplay="autoplay"
+        	document.getElementById("video").src=""
+        }
+         function videoseven()
+        {
+        	document.getElementById("video").autoplay="autoplay"
+        	document.getElementById("video").src=""
+        }
+         function videoeight()
+        {
+        	document.getElementById("video").autoplay="autoplay"
+        	document.getElementById("video").src=""
+        }
+         function videonine()
+        {
+        	document.getElementById("video").autoplay="autoplay"
+        	document.getElementById("video").src=""
+        }
+         function videoten()
+        {
+        	document.getElementById("video").autoplay="autoplay"
+        	document.getElementById("video").src=""
+        }
+         function videoeleven()
+        {
+        	document.getElementById("video").autoplay="autoplay"
+        	document.getElementById("video").src=""
+        }
+         function videotwelve()
+        {
+        	document.getElementById("video").autoplay="autoplay"
+        	document.getElementById("video").src=""
+        }
+        function videothirteen()
+        {
+        	document.getElementById("video").autoplay="autoplay"
+        	document.getElementById("video").src=""
+        }
+        function videofourteen()
+        {
+        	document.getElementById("video").autoplay="autoplay"
+        	document.getElementById("video").src=""
+        }
