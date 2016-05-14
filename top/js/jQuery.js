b4e15ba2b18a3f5262333773bfd0cd7b9d9c2001
lzyan1102 <lzyan1102@163.com>
2016-05-14 08:52:32 +0800

    Add files via upload

---
+++ top/js/jQuery.js
@@ -0,0 +1,15 @@
+        function  JumpToweibo()
+        {
+        	var system ={};  
+   			var p = navigator.platform;       
+    		system.win = p.indexOf("Win") == 0;  
+  	        system.mac = p.indexOf("Mac") == 0;  
+  		    system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);     
+   		    if(system.win||system.mac||system.xll)
+			{//pc 
+     		  window.location.href="http://www.weibo.com/u/3865190713/";  
+   			 }else
+			{//moble
+     		  window.location.href="http://m.weibo.cn/u/3865190713/";  
+   			 }
+       }
